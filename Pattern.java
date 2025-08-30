0cfa015e8ff5522c0f488b6921d373b560e50fb4
Aditya Pratap Singh <kumar183408@gmail.com>
2025-08-30 10:29:35 +0530

    Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -9,3 +9,52 @@ class patternn{
         }
     }
 }
+
+#Next Trangle
+class patternn{
+    public static void main(String args[]){
+        int a=5;
+        for(int i=0; i<=a; i++){
+            for(int j=0; j<=5-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+#Next Trangle
+class patternn{
+    public static void main(String args[]){
+        int a=5;
+        for(int i=0; i<=a; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=5-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+#Next Trangle..
+class patternn{
+    public static void main(String args[]){
+        int a=5;
+        for(int i=0; i<=a; i++){
+            for(int j=0; j<=5-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+    
